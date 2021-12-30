d23be699376903eb7aa74953d3319ed9a5bfca08
zerai <teclaizerai@gmail.com>
2021-12-30 11:29:25 +0100

    [blue] composer: agg. test-pack, ci conf e placeholder test.

---
+++ blue/tests/CiPlaceholderTest.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class CiPlaceholderTest extends TestCase
+{
+    public function testCiPlaceholder():void
+    {
+        self::assertTrue(true);
+    }
+}
