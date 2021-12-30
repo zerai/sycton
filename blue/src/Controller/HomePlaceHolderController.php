36ea541efa44deee5ec2604e107e8d88d86c4c53
zerai <teclaizerai@gmail.com>
2021-12-30 17:27:59 +0100

    [Blue] home placeholder controller.

---
+++ blue/src/Controller/HomePlaceHolderController.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Domain\VisitorRegistrationApplication;
+use Ecotone\Modelling\CommandBus;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomePlaceHolderController extends AbstractController
+{
+
+    #[Route("/", name: "home", methods: ["GET"])]
+    public function register(Request $request): Response
+    {
+        return new JsonResponse(["serviceName" => "Blue service"]);
+    }
+}
