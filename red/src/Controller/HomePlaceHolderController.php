db8707652376b193add85fe377f312565513ff82
zerai <teclaizerai@gmail.com>
2022-01-23 11:37:15 +0100

    [red] fix CS.

--- red/src/Controller/HomePlaceHolderController.php
+++ red/src/Controller/HomePlaceHolderController.php
@@ -2,21 +2,19 @@
 
 namespace App\Controller;
 
-use App\Domain\VisitorRegistrationApplication;
-use Ecotone\Modelling\CommandBus;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomePlaceHolderController extends AbstractController
 {
-
     #[Route("/", name: "home", methods: ["GET"])]
     public function register(Request $request): Response
     {
-        return new JsonResponse(["serviceName" => "Red service"]);
+        return new JsonResponse([
+            "serviceName" => "Red service",
+        ]);
     }
 }
