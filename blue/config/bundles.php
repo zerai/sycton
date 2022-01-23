44d72ab2a19875b9fe7e849c7c0309f4e7be0f5f
zerai <teclaizerai@gmail.com>
2022-01-23 10:07:14 +0100

    [blue] fix config/bundle.php.

--- blue/config/bundles.php
+++ blue/config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Ecotone\SymfonyBundle\EcotoneSymfonyBundle::class => ['all' => true],
 ];
