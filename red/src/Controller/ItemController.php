243b9a6be02fa13cbdb1b4b694f277e1e7be0f62
zerai <teclaizerai@gmail.com>
2022-04-16 11:37:54 +0200

    red - add base item.

---
+++ red/src/Controller/ItemController.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller;
+
+use Ecotone\Modelling\CommandBus;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ItemController extends AbstractController
+{
+    public function __construct(private CommandBus $commandBus) {}
+
+    #[Route("/items", methods: ["POST"])]
+    public function register(Request $request): Response
+    {
+        $name = $request->get("name");
+
+        $this->commandBus->sendWithRouting("registerItem", $name);
+
+        return new RedirectResponse("/");
+    }
+}
