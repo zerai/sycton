e5b0f45aab8892777ab73894968bbee0f0613a16
zerai <teclaizerai@gmail.com>
2022-04-16 11:33:41 +0200

    red - add orm and db config.

--- red/config/bundles.php
+++ red/config/bundles.php
@@ -3,4 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Ecotone\SymfonyBundle\EcotoneSymfonyBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
