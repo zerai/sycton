f3a94af608cce42618718abeaf00c7d0fb18b0ee
zerai <teclaizerai@gmail.com>
2022-06-01 13:02:10 +0200

    [Blue][Composer] added dama/doctrine-test-bundle.

--- blue/config/bundles.php
+++ blue/config/bundles.php
@@ -6,4 +6,5 @@ return [
     Ecotone\SymfonyBundle\EcotoneSymfonyBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
