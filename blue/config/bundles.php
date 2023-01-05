40f24489c4976fcd134774a7aa451c10fb2422e6
zerai <teclaizerai@gmail.com>
2023-01-05 10:58:41 +0100

    [blue] composer added spomky-labs/lexik-jose-bridge web-token/jwt-signature-algorithm-rsa annotations.

--- blue/config/bundles.php
+++ blue/config/bundles.php
@@ -10,4 +10,8 @@ return [
     Manyou\WorkermanSymfonyRuntime\WorkermanSymfonyRuntimeBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Http\HttplugBundle\HttplugBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Jose\Bundle\JoseFramework\JoseFrameworkBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    SpomkyLabs\LexikJoseBundle\SpomkyLabsLexikJoseBundle::class => ['all' => true],
 ];
