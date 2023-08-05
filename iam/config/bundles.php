66e0359b3a0c072d965d28418c90396fc37f74da
zerai <teclaizerai@gmail.com>
2023-08-05 17:15:34 +0200

    IAM: composer removed dama/doctrine-test-bundle.

--- iam/config/bundles.php
+++ iam/config/bundles.php
@@ -9,7 +9,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     League\Bundle\OAuth2ServerBundle\LeagueOAuth2ServerBundle::class => ['all' => true],
     Ecotone\SymfonyBundle\EcotoneSymfonyBundle::class => ['all' => true],
 ];
