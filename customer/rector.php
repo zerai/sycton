79379e40d1f9729c39ea9f6d0a3bde2baa60c08c
zero <teclaizerai@gmail.com>
2023-08-06 14:53:11 +0200

    customer service init (#642)
    * Copy red to customer.
    
    * Update docker-compose conf.
    
    * CI: fix validate openApi-schema job.
    
    * Composer: update script section.
    
    * Composer:added customer namespace.
    
    * Conf. symfony (container|env file).
    
    * Conf. phpunit.
    
    * Conf. tools (ecs,psalm,rector).

---
+++ customer/rector.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\Config\RectorConfig;
+use Rector\Core\ValueObject\PhpVersion;
+use Rector\Doctrine\Set\DoctrineSetList;
+use Rector\PHPUnit\Set\PHPUnitSetList;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Symfony\Set\SymfonySetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        //__DIR__ . '/config',
+        //__DIR__ . '/migrations',
+        //__DIR__ . '/public',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+        __DIR__ . '/_customer/src',
+        __DIR__ . '/_customer/tests',
+
+    ]);
+
+    $rectorConfig->skip([
+        __DIR__ . '/src/Kernel.php',
+        __DIR__ . '/src/Controller',
+        __DIR__ . '/src/Domain',
+        __DIR__ . '/src/Entity',
+    ]);
+
+    $rectorConfig->phpVersion(PhpVersion::PHP_81);
+    $rectorConfig->symfonyContainerXml(__DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml');
+
+    // register a single rule
+    //$rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
+
+    // define sets of rules
+    $rectorConfig->sets([
+        /**
+         * PHP
+         */
+        LevelSetList::UP_TO_PHP_81,
+
+        /**
+         * SYMFONY
+         */
+        SymfonySetList::SYMFONY_61,
+        SymfonySetList::SYMFONY_CODE_QUALITY,
+
+        /**
+         * DOCTRINE
+         */
+        DoctrineSetList::DOCTRINE_DBAL_30,
+        DoctrineSetList::DOCTRINE_ORM_29,
+        DoctrineSetList::DOCTRINE_CODE_QUALITY,
+
+        /**
+         * PHPUNIT
+         */
+        PHPUnitSetList::PHPUNIT_91,
+    ]);
+};
---
+++ customer/src/Controller/HomePlaceHolderController.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomePlaceHolderController extends AbstractController
+{
+    #[Route("/", name: "home", methods: ["GET"])]
+    public function register(Request $request): Response
+    {
+        return new JsonResponse([
+            "serviceName" => "Red service",
+        ]);
+    }
+}
