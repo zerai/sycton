ed74c80931a4dd1c0fbc9e78a35055eda99f76f8
zerai <teclaizerai@gmail.com>
2024-04-06 14:58:46 +0200

    Blue: composer added symfony/profiler-pack.

--- blue/config/bundles.php
+++ blue/config/bundles.php
@@ -8,4 +8,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Manyou\WorkermanSymfonyRuntime\WorkermanSymfonyRuntimeBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
