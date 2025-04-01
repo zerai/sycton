dce1b530ccfc0deb5da0ff93b5cde7d893b25983
zero <teclaizerai@gmail.com>
2025-04-01 12:22:18 +0200

    Red: added workerman runtime.

--- red/config/bundles.php
+++ red/config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Luzrain\WorkermanBundle\WorkermanBundle::class => ['all' => true],
 ];
